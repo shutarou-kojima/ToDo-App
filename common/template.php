89dee27e4c15187023aecbfdb090f33f353ec34e
shutarou-kojima <shutarou.kojima@gmail.com>
2021-09-28 22:18:55 +0900

    add file:common

---
+++ common/template.php
@@ -0,0 +1,24 @@
+<?php
+
+function templateHTML($css, $js, $title, $h1, $nav, $main)
+{
+  echo
+"<!DOCTYPE html>
+<html lang='ja'>
+<head>
+  <meta charset='utf-8'>
+  <link type='text/css' rel='stylesheet' href='/common/sanitize.css' />
+  $css
+  $js
+  <title>$title</title>
+</head>
+
+<body>
+  <header>
+    <h1>$h1</h1>
+    <nav>$nav</nav>
+  </header>
+  <main>$main</main>
+</body>
+</html>";
+}
--- sandbox.php
+++ sandbox.php
@@ -1,16 +1,11 @@
 <?php
-  require_once '/MAMP/htdocs/todo-app/template.php';
+  require_once '/MAMP/htdocs/todo-app/common/template.php';
+  $css = "<link rel='stylesheet' href='./style.css'>";
+  // $js = "<script type='text/javascript' src='./script.js'></script>";
+  $js = "";
   $title = "サンドボックスのページ";
   $h1 = "サンドボックス";
-  
-  function nav()
-  {
-    echo "<button>ボタン</button>";
-  }
+  $nav = "<button>ボタン</button>";
+  $main = "<p>テキスト<br>テキストテキスト</p>";
 
-  function main()
-  {
-    echo "<p>テキスト<br>テキストテキスト</p>";
-  }
-
-  templateHTML($title, $h1, "nav", "main");
+  templateHTML($css, $js, $title, $h1, $nav, $main);
